1c593382721baebe1a4387b854ae457cbd3b3ecd
Srikanth Pelluru <Srikanth.Pelluru@sapiens.com>
2023-11-25 01:49:13 +0530

    updated service url

--- src/app/constants/app-constants.ts
+++ src/app/constants/app-constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants {
-    public static SERVICE_BASE_URL="http://localhost:8989/";
+    public static SERVICE_BASE_URL="https://youtube-services.onrender.com/";
     public static EMBED_YT_BASE_URL="https://www.youtube.com/embed/";
 }
